96283c5f677852c9db2de65373c1921baeb06aa6
HectorMtz22 <hec.gre81@gmail.com>
2021-02-17 19:47:39 -0600

    Some comments

--- c1_examples/fahr_to_celsius.c
+++ c1_examples/fahr_to_celsius.c
@@ -10,8 +10,14 @@ int main() {
 
 	fahr = lower; // This is the initialization
 	
+	/* We have a critical operation
+	 * This is because we declared our variables as "integers"
+	 * If I put 5/9 automatically it becomes 0 (Because Integer)
+	 * Proceed with CAUTION
+	*/
+
 	while (fahr <= upper) {
-		celsius = 5 * (fahr - 32) / 9;
+		celsius = 5 * (fahr - 32) / 9; // This is a critical operation OJITO
 		printf("%d\t%d\n", fahr, celsius);
 		fahr += step; // I wrote this like in JavaScript and it works!
 	}
