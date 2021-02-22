74bc87797f47017019edc1f3b1462708755c37fc
HectorMtz22 <hec.gre81@gmail.com>
2021-02-22 17:13:07 -0600

    Add Comments

--- c1_examples/word_counting.c
+++ c1_examples/word_counting.c
@@ -1,24 +1,25 @@
 #include <stdio.h> // printf
 
+/* State "In a word" statement */
 #define IN 1 
 #define OUT 0
 
 int main() {
-	int c, nl, nw, nc, state;
+	int c, nl, nw, nc, state; // Declaration
 	
-	state = OUT;
-	nl = nw = nc = 0;
+	state = OUT; // Initial Values
+	nl = nw = nc = 0; // Initialize Variables with 0
 
 	while ((c = getchar()) != EOF) {
 		++nc; 
-		if (c == '\n') {
-			++nl;
+		if (c == '\n') { // If c is a new line
+			++nl; // Newline + 1
 		}
 		if (c == ' ' || c == '\n' || c == '\t') {
-			state = OUT;
+			state = OUT; // It isn't in a word
 		} else if (state == OUT) {
-			state = IN;
-			++nw;
+			state = IN;  // It is in a word
+			++nw; // New Word count + 1
 		}
 	}
 	printf("%d %d %d\n", nl, nw, nc);
