386f9b052f430592da882cdabef35075f47430c3
HectorMtz22 <hec.gre81@gmail.com>
2021-02-22 18:25:13 -0600

    Make the histogram a Function and return 0

--- c1_examples/decimal_counting.c
+++ c1_examples/decimal_counting.c
@@ -1,13 +1,16 @@
 #include <stdio.h> 
 
+int histogram(int ndigit[]);
+
 int main() {
-	int c, i, j, k, nwhite, nother;
+	int c, i, nwhite, nother;
 	int ndigit[10];
 
 	nwhite = nother = 0;
 	for (i = 0; i < 10; i++) {
 		ndigit[i] = 0;
 	}
+
 
 	while ((c = getchar()) != EOF) {
 		if (c >= '0' && c <= '9') {
@@ -19,6 +22,19 @@ int main() {
 		}
 	}
 
+	histogram(ndigit);
+
+	printf("digits =");
+	for (i = 0; i < 10; i++) {
+		printf(" %d", ndigit[i]);
+	}
+	printf(", white space = %d, other = %d\n", nwhite, nother);
+	return 0;
+}
+
+int histogram(int ndigit[]) {
+	int j, k;
+
 	// Histogram
 	printf("\nHistogram\n");
 	for (j = 0; j < 10; j++) {
@@ -29,9 +45,5 @@ int main() {
 		printf("\n");
 	}
 
-	printf("digits =");
-	for (i = 0; i < 10; i++) {
-		printf(" %d", ndigit[i]);
-	}
-	printf(", white space = %d, other = %d\n", nwhite, nother);
+	return 0;
 }
