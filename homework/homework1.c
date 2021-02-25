31531be6d84c1594ec3092dac56f475824b30103
HectorMtz22 <hec.gre81@gmail.com>
2021-02-25 03:14:26 -0600

    Eureka

--- homework/homework1.c
+++ homework/homework1.c
@@ -3,46 +3,48 @@
 #include <ctype.h>
 #define MAX 80
 
-int convert_to_line();
-int ignore_tags();
-int check_tag();
-int print();
+int convert_to_line(); // Converts single characters to lines
+// int ignore_tags(); 
+// int check_tag(); 
+int print(); // Prints the line w/o the tags
 int spacesToUnderscore();
 char linea[MAX];
 
 int main() {
 	extern char linea[];
 	int ignore = 1;
-	int tag = 0;
 	int p, strong, h1;
-	p = strong = h1 = 0;
+	
+	p = strong = h1 = 0; // Initialization
 	while (convert_to_line() == 0) {
+
+		// I should use switch case 
 		if (strstr(linea, "<p>") != NULL) {
 			p++;
 		}
+		if (strstr(linea, "<h1>") != NULL) {
+			h1++;
+		}
+		if (h1 > 0) {
+			spacesToUnderscore();
+		}
+		if (strstr(linea, "</h1") != NULL) {
+			h1--;
+		}
 		if (p > 0) {
-			print();
+			if (strstr(linea, "<strong>") != NULL) {
+				strong++;
+				spacesToUnderscore();
+			} else {
+				print();
+			}
 		}
 		if (strstr(linea, "</p>") != NULL) {
 			p--;
 		}
-		if (strstr(linea, "<strong>") != NULL) {
-			strong++;
-		}
 		if (strstr(linea, "</strong>") != NULL) {
 			strong--;
 		}
-	}
-	return 0;
-}
-
-int check_tag() {
-	extern char linea[];
-	if (strstr(linea, "<p>") != NULL) {
-		return 1;
-	}
-	if (strstr(linea, "</p>") != NULL) {
-		return 0;
 	}
 	return 0;
 }
@@ -55,16 +57,13 @@ int print() {
 
 	for (i = 0; i < num_max; i++) {
 		if (linea[i] == '<') {
-			if (strstr(linea, "strong") != NULL) {
-				spacesToUnderscore();
-			}
 			ignore = 1;
 		} 
 		if (!ignore) {
-			
 			printf("%c", linea[i]);
 		}
 		if (linea[i] == '>') {
+			printf("\n");  // New line for all 
 			ignore = 0;
 		}
 	}
@@ -74,22 +73,58 @@ int spacesToUnderscore() {
 	extern char linea[];
 	int space;
 	int i;
-	for (i = 0; i < MAX ; i++) {
-    if (isspace(linea[i])) {
+	int ignore = 1;
+	int num_max = strlen(linea); // This functions counts the length
+
+	for (i = 0; i < num_max; i++) {
+		if (linea[i] == '<') { // Here, we evaluate the char 
+			ignore = 1;
+		} 
+		if (!ignore) {
+			if (isspace(linea[i])) {
       space = 1;
     } else {
       if (space)  {
-				printf("_");
-				space = 0;
+				printf("_"); // Replace the space
+				space = 0;   // Reestablecer counter
       }
-    printf("%c", linea[i]);
-		// putchar(linea[i]);
-  	}
+			printf("%c", linea[i]);
+		}
+		}
+		if (linea[i] == '>') {
+			ignore = 0;
+		}
 	}
   return 0;
 
 }
 
+int convert_to_line() {
+  extern char linea[];
+  int c, i = 0;
+  for (i = 0; i < MAX  && (c = getchar()) != EOF && c != '\n';) {
+      linea[i++] = c;
+  }
+  linea[i] = '\0'; 
+	return c == EOF;
+}
+
+// These functions were the experiments
+
+/*
+int check_tag() {
+	extern char linea[];
+	if (strstr(linea, "<p>") != NULL) {
+		return 1;
+	}
+	if (strstr(linea, "</p>") != NULL) {
+		return 0;
+	}
+	return 0;
+}
+*/
+
+/*
 int ignore_tags() {
 	extern char linea[];
 	int ignore = 0;
@@ -108,14 +143,4 @@ int ignore_tags() {
 		}
 	}
 }
-
-int convert_to_line() {
-  extern char linea[];
-  int c, i = 0;
-  int sc = 0;
-  for (i = 0; i < MAX  && (c = getchar()) != EOF && c != '\n';) {
-      linea[i++] = c;
-  }
-  linea[i] = '\0'; // caracter de terminacion de string
-  return c == EOF;
-}
+*/
