76da01e60c3a8457a5cb83f3131ed9ab13f9fbf4
HectorMtz22 <hec.gre81@gmail.com>
2021-02-25 03:31:17 -0600

    Add h2, h3, h4, h5, h6

--- homework/homework1.c
+++ homework/homework1.c
@@ -13,24 +13,26 @@ char linea[MAX];
 int main() {
 	extern char linea[];
 	int ignore = 1;
-	int p, strong, h1;
+	int p, strong, h1, h2, h3, h4, h5, h6;
 	
 	p = strong = h1 = 0; // Initialization
 	while (convert_to_line() == 0) {
 
 		// I should use switch case 
-		if (strstr(linea, "<p>") != NULL) {
-			p++;
-		}
-		if (strstr(linea, "<h1>") != NULL) {
-			h1++;
-		}
-		if (h1 > 0) {
-			spacesToUnderscore();
-		}
-		if (strstr(linea, "</h1") != NULL) {
-			h1--;
-		}
+		strstr(linea, "<p>") != NULL ? p++ : 0;
+		strstr(linea, "<h1>") != NULL ? h1++ : 0;
+		strstr(linea, "<h2>") != NULL ? h2++ : 0;
+		strstr(linea, "<h3>") != NULL ? h3++ : 0;
+		strstr(linea, "<h4>") != NULL ? h4++ : 0;
+		strstr(linea, "<h5>") != NULL ? h5++ : 0;
+		strstr(linea, "<h6>") != NULL ? h6++ : 0;
+		h1 > 0 || h2 > 0 || h3 > 0 || h4 > 0 || h5 > 0 || h6 > 0 ? spacesToUnderscore() : 0 ;
+		strstr(linea, "</h1>") != NULL ? h1-- : 0;
+		strstr(linea, "</h2>") != NULL ? h2-- : 0;
+		strstr(linea, "</h3>") != NULL ? h3-- : 0;
+		strstr(linea, "</h4>") != NULL ? h4-- : 0;
+		strstr(linea, "</h5>") != NULL ? h5-- : 0;
+		strstr(linea, "</h6>") != NULL ? h6-- : 0;
 		if (p > 0) {
 			if (strstr(linea, "<strong>") != NULL) {
 				strong++;
