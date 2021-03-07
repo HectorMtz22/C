cf428e1ba4641f0cbca09ad2fda3efc02dfa7183
HectorMtz22 <hec.gre81@gmail.com>
2021-03-06 18:26:42 -0600

    Sth weird when the input is only a number

--- homework/homework3.c
+++ homework/homework3.c
@@ -6,7 +6,8 @@
 
 int main() {
 	int c;
-	int parenthesis;
+	int parenthesis, brackets, braces, single_q, double_q;
+	int last_digit = OUT;
 
 
 	while((c = getchar()) != EOF) {
@@ -17,10 +18,33 @@ int main() {
 			case ')':
 				parenthesis--;
 				break;
+			case '[':
+				brackets++;
+				break;
+			case ']':
+				brackets--;
+				break;
+			case '{':
+				braces++;
+				break;
+			case '}':
+				braces--;
+				break;
+			case '\'':
+				single_q = !single_q;
+				break;
+			case '\"':
+				double_q = !double_q;
+				break;
 		}
-		if (!parenthesis) {
-			isdigit(c) ? putchar('x') : putchar(c);
-		} else {
+		if (!parenthesis || !brackets || !braces || !single_q || !double_q) {
+			isdigit(c) ? last_digit = IN : putchar(c);
+			if (!isdigit(c) && last_digit == IN) {
+				putchar('x');
+				last_digit = OUT;
+			}
+		}
+		else {
 			putchar(c);
 		}
 	}
