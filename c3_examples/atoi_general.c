406a78ff3743061849f050855495cbd63c010e2b
HectorMtz22 <hec.gre81@gmail.com>
2021-03-23 18:38:01 -0600

    General atoi

---
+++ c3_examples/atoi_general.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int atoi_general(char s[]) {
+	int i, n, sign;
+
+	for (i = 0; isspace(s[i]); i++);
+
+	sign = (s[i] == '-') ? -1: 1;
+	if (s[i] == '+' || s[i] == '-') {
+		i++;
+	}
+
+	for (n = 0; isdigit(s[i]); i++) {
+		n = 10 * n + (s[i] - '0');
+	}
+
+	return sign * n;
+
+}
+
+int main(int x, char** argc) {
+	char* s;
+	int result; 
+
+
+	result = atoi_general(argc[1]);
+	printf("%d\n", result);
+	return 0;
+}
