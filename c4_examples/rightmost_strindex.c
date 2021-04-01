5bc49a5800dd956fa57607d7bbefd8b3b104f346
HectorMtz22 <hec.gre81@gmail.com>
2021-04-01 17:59:46 -0600

    Ex 4-1

---
+++ c4_examples/rightmost_strindex.c
@@ -0,0 +1,24 @@
+#include <stdio.h> // i/o
+
+#define END '\0'
+
+int strindex(char s[], char t[]) {
+	int i, j, k;
+
+	for (i = 0; s[i] != END; i++) {
+		for (j = i, k = 0; t[k] != END && s[j] == t[k]; j++, k++);
+		if (k > 0 && t[k] == END) {
+			return i + (k - 1);
+		}
+	}
+	return -1;
+}
+
+int main() {
+	char s[] = "Hola, esto es una prueba";
+	char t[] = "prueba";
+	int res;
+
+	res = strindex(s, t);
+	printf("%d\n", res);
+}
