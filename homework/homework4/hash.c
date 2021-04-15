a0dd4f672f61c839a8b5a8a0a82dc09b2e6503aa
HectorMtz22 <hec.gre81@gmail.com>
2021-04-15 13:17:17 -0500

    More comments

--- homework/homework4/hash.c
+++ homework/homework4/hash.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
+// Structure for hash table
 struct SaveAssign {
 	char key[100];
 	double value[100];
 };
 
 int count = 0;
-struct SaveAssign newAssign;
+struct SaveAssign newAssign; // newAssign is the hash table
 
+// push the respective key with value
 void pushAssign(char key, double value) {
+	// Assign the key with value
 	newAssign.key[count] = key;
 	newAssign.value[count] = value;
 	count++;
 }
 
+// read if a char match with a value
 int readAssign(char f) {
 	int i;
 
