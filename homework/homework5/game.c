0ea7a76dbd630d570e3784d7bd6d4b8dec61f462
HectorMtz22 <hec.gre81@gmail.com>
2021-04-22 02:34:22 -0500

    It works

--- homework/homework5/game.c
+++ homework/homework5/game.c
@@ -1,9 +1,10 @@
 #include "homework5.h"
+extern int **matrix1;
+extern int **matrix2;
+extern int status;
+extern int size;
 
 int state(int let, int num) {
-	extern int **matrix1;
-	extern int **matrix2;
-	extern int status;
 	
 	let -= 48;
 	let--;
@@ -15,4 +16,20 @@ int state(int let, int num) {
 		matrix2[num][let] = 1;
 	}
 	return 0;
+}
+
+
+void detect_blanks(int i, int j) {
+	if (matrix1[i][j] == 0) {
+		matrix2[i][j] = 1;
+		if (i < size) {
+			detect_blanks(i++, j);
+		}
+		if (j < size) {
+			detect_blanks(0, j++);
+		}
+	} else {
+		return;
+	}
+	return;
 }
--- homework/homework5/homework5.c
+++ homework/homework5/homework5.c
@@ -26,6 +26,8 @@ int main(int argc, char** args) {
 	rellenar();
 
 	showDiff();
+	printf("Type a column with a row\n");
+	printf("If you type '=', it will capture the mine with a flag\n");
 	while ((type = getop(s)) != EOF) {
 		switch (type) {
 			case NUMBER:
