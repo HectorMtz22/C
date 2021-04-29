be9435f825256e047c4610eed2aa91751eec9a6c
HectorMtz22 <hec.gre81@gmail.com>
2021-04-28 19:46:05 -0500

    no se que hice

--- homework/homework6/homework6.c
+++ homework/homework6/homework6.c
@@ -8,22 +8,32 @@ char linea[MAX];
 char ins[MAX];
 
 int main() {
-	int c;
+	int c, i;
 	node* n = NULL;
 	node* arbolito = NULL;
 	char username[MAX];
 	char password[MAX];
+	char temp1[MAX]; 
+	char temp2[MAX];
 
 	while((convert_to_line(username, password)) != EOF ) {
 		if (strstr(ins, "add")) {
-			printf("%s %s\n", username, password);
-			n = insert(username, password, arbolito, 0);
+			i = 0;
+			while (i != '\0') {
+				temp1[i] = username[i];
+				temp2[i] = password[i];
+				i++;
+			}
+
+			n = insert(temp1, temp2, arbolito, 0);
 			if (arbolito == NULL) {
 				arbolito = n;
 			}
 		} else if (strstr(ins, "del")) {
+			
 			show(arbolito, '+');
 		} else {
+		printf("%s\n", arbolito->user);
 			show(arbolito, '+');
 		}
 	}
