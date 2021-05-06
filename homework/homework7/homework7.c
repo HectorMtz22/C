e75e3ff503f51e4e77b11f5fde321a91547ad10d
HectorMtz22 <hec.gre81@gmail.com>
2021-05-06 10:38:45 -0500

    Add structs

--- homework/homework7/homework7.c
+++ homework/homework7/homework7.c
@@ -8,11 +8,18 @@
 #include <time.h>
 #include "homework7.h"
 
+int adminStatus = 0;
 
 int main() {
+    FILE * database;
     long int tp = time(0);
     struct tm * normalTime = localtime(&tp);
     welcome(normalTime);
-    printf("%d", isEmployee());
+    if (isEmployee()) {
+        database = fopen("admin.txt", "r");
+        fclose(database);
+    } else {
+        fopen("employees.txt", "a");
+    }
     return 0;
 }
